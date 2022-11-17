7c89a56d2e0492f8b527d9cbbff6ee2c41f78136
AbderrahimTroudi <youssef2001-11@hotmail.com>
2022-11-17 21:33:25 +0100

    Added new files

---
+++ precache-manifest.2d9dd721308a39f79977acae23da0de3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea72ff9e7a64bcf8379c",
+    "url": "./static/css/main.55d210aa.chunk.css"
+  },
+  {
+    "revision": "ea72ff9e7a64bcf8379c",
+    "url": "./static/js/main.ea72ff9e.chunk.js"
+  },
+  {
+    "revision": "4f656f5fde676063d3a6",
+    "url": "./static/js/1.4f656f5f.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "f29a661498ef1f6c6697389798527d3c",
+    "url": "./static/media/cv-pfe.f29a6614.pptx"
+  },
+  {
+    "revision": "1db1c7c706be958a88b2459c9ee8d523",
+    "url": "./static/media/me1.1db1c7c7.png"
+  },
+  {
+    "revision": "d1e8fb803162800c55cbc18b57899262",
+    "url": "./static/media/portfolio1.d1e8fb80.jpg"
+  },
+  {
+    "revision": "ce79413f3dfb4b9ab668d3236a4a04d1",
+    "url": "./static/media/portfolio3.ce79413f.jpg"
+  },
+  {
+    "revision": "04dbe3ab7df1162d6c760d973af4ad6a",
+    "url": "./static/media/portfolio4.04dbe3ab.jpg"
+  },
+  {
+    "revision": "cce858cbb7117ac54d47d84961b04314",
+    "url": "./static/media/SecondImg.cce858cb.jpg"
+  },
+  {
+    "revision": "13db5e2202aa39b783fded70d877b662",
+    "url": "./static/media/portfolio2.13db5e22.jpg"
+  },
+  {
+    "revision": "c6e34db9e36b84eb604b3d8897e2b5c1",
+    "url": "./static/media/portfolio5.c6e34db9.png"
+  },
+  {
+    "revision": "32a902840cca71090c80890561f4be7f",
+    "url": "./static/media/portfolio6.32a90284.jpg"
+  },
+  {
+    "revision": "55591b46c0cfff3897c8354ee84e594b",
+    "url": "./index.html"
+  }
+];
