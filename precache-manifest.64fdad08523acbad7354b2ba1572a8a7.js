99be6d3c15bca44e7d27e8ce31adebcfa464448b
AbderrahimTroudi <youssef2001-11@hotmail.com>
2023-10-22 00:31:28 +0200

    new_update

---
+++ precache-manifest.64fdad08523acbad7354b2ba1572a8a7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "a63f04c3c984bf708f52",
+    "url": "./static/css/main.9ae1668c.chunk.css"
+  },
+  {
+    "revision": "a63f04c3c984bf708f52",
+    "url": "./static/js/main.a63f04c3.chunk.js"
+  },
+  {
+    "revision": "0478ca5d2b2af45779ca",
+    "url": "./static/js/1.0478ca5d.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "633693b6f5c6a4b362866ddec952de6f",
+    "url": "./static/media/taxi.633693b6.jpg"
+  },
+  {
+    "revision": "0971f839730f45a30c4b1fab9c566ac8",
+    "url": "./static/media/MonCV.0971f839.pdf"
+  },
+  {
+    "revision": "1db1c7c706be958a88b2459c9ee8d523",
+    "url": "./static/media/me1.1db1c7c7.png"
+  },
+  {
+    "revision": "14b01752c9c8ac98047cde3d511479fc",
+    "url": "./static/media/javacapt.14b01752.PNG"
+  },
+  {
+    "revision": "d1e8fb803162800c55cbc18b57899262",
+    "url": "./static/media/portfolio1.d1e8fb80.jpg"
+  },
+  {
+    "revision": "83905c404ee32c20cdd2ef289b6a95ee",
+    "url": "./static/media/portcap.83905c40.PNG"
+  },
+  {
+    "revision": "cce858cbb7117ac54d47d84961b04314",
+    "url": "./static/media/SecondImg.cce858cb.jpg"
+  },
+  {
+    "revision": "28f36cd680996df783b0d788389a2f62",
+    "url": "./index.html"
+  }
+];
