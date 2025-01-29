a856b67f010d4ba43286c6dff5b92d559b6b5b00
AbderrahimTroudi <youssef2001-11@hotmail.com>
2025-01-29 20:33:04 +0100

    fix OI762QA

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,7 @@ const nextConfig = {
   images: {
     unoptimized: true, // Disable image optimization for GitHub Pages
   },
-  basePath: '/troudi-abderrahim.github.io',
-  assetPrefix: '/troudi-abderrahim.github.io',
+
 };
 
 export default nextConfig;
