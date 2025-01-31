41a00744b1e6e5da9a1b9aeabd8230d7248d459b
AbderrahimTroudi <youssef2001-11@hotmail.com>
2025-01-31 16:01:13 +0100

    fixed next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
+
   images: {
     unoptimized: true, // Disable image optimization for GitHub Pages
   },
-  basePath: '/troudi-abderrahim.github.io',
-  assetPrefix: '/troudi-abderrahim.github.io',
+
 };
 
 export default nextConfig;
